d2b0f43d4de5a9a9a5838c1fe2ddc2f578737471
JromarioSs <jromario2014@gmail.com>
2023-09-17 09:29:28 -0300

    js 03 javascript consumo de api finalizado

--- javascript/03JavaScriptConsumindoTratandoDadosAPI/script.js
+++ javascript/03JavaScriptConsumindoTratandoDadosAPI/script.js
@@ -1,16 +1,31 @@
 async function buscaEndereco(cep) {
+  let msgError = document.getElementById('erro');
+  msgError.innerHTML = '';
+
   try {
     let consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
     let res = await consultaCEP.json();
+
     if (res.erro) {
       throw Error("Cep não existe...");
     }
-    return res;
+
+    let cidade = document.getElementById('cidade');
+    let logradouro = document.getElementById('endereco');
+    let estado = document.getElementById('estado');
+
+    cidade.value = res.localidade;
+    logradouro.value = res.logradouro;
+    estado.value = res.uf;
+
+    console.log(res)
   } catch(err) {
+    msgError.innerHTML += `<p>Cep não existe...</p>`;
     console.log(err);
   }
 }
 
-let ceps = ['01001000', '01001001'];
-let cepsConj = ceps.map(valor => buscaEndereco(valor));
-Promise.all(cepsConj).then(res => console.log(res));
+let cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => {
+  buscaEndereco(cep.value)
+})
